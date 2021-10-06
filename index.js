cd449fe7e3268cb252c39b8237ca578509e3fb92
rosmith22 <smithrukayat1@gmai.com>
2021-10-06 14:27:40 +0100

    My first counter commit

---
+++ index.js
@@ -0,0 +1,91 @@
+const btns =document.querySelectorAll(".btn");
+const value =document.querySelector(".value");
+const header =document.querySelector(".header")
+
+let count = 0;
+
+btns.forEach((btn) => {
+    btn.addEventListener("click",Actions);
+    
+});
+
+function Actions(e){
+    const styles = e.currentTarget.classList;
+
+    if(styles.contains("increase")){
+        if(count < 100){
+            count++;
+        }
+    }
+        else if(styles.contains("decrease")){
+            if (count >= 1){
+                count--;
+            }
+        }
+        else{
+            count=0;
+        }
+        if (count > 0) {
+                value.style.color = "#93d823";
+                header.style.color = "#93d823";
+              }
+            
+              if (count < 0) {
+                value.style.color = "#e4215c";
+                header.style.color = "#e4215c";
+              }
+            
+              if (count === 0) {
+                value.style.color = "#22436c";
+                header.style.color = "#22436c";
+              }
+            
+              value.textContent = count;
+            }
+     
+    
+    
+
+
+// const btns = document.querySelectorAll(".btn");
+// const value = document.querySelector(".value");
+// const header = document.querySelector(".header");
+
+// let count = 0;
+
+// btns.forEach((btn) => {
+//   btn.addEventListener("click", Actions);
+// });
+
+// function Actions(e) {
+//   const styles = e.currentTarget.classList;
+
+//   if (styles.contains("increase")) {
+//     if (count < 100) {
+//       count++;
+//     }
+//   } else if (styles.contains("decrease")) {
+//     if (count >= 1) {
+//       count--;
+//     }
+//   } else {
+//     count = 0;
+//   }
+
+//   if (count > 0) {
+//     value.style.color = "#93d823";
+//     header.style.color = "#93d823";
+//   }
+
+//   if (count < 0) {
+//     value.style.color = "#e4215c";
+//     header.style.color = "#e4215c";
+//   }
+
+//   if (count === 0) {
+//     value.style.color = "#22436c";
+//     header.style.color = "#22436c";
+//   }
+
+//   value.textContent = count;
+// }
